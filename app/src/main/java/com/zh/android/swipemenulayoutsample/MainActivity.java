f4b5eb37ec77e204fba6d43b89b09adcd5b41137
hezihao <he100zihao100@126.com>
2022-07-28 11:21:45 +0800

    update:增加一些注释

--- app/src/main/java/com/zh/android/swipemenulayoutsample/MainActivity.java
+++ app/src/main/java/com/zh/android/swipemenulayoutsample/MainActivity.java
@@ -17,16 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final SwipeMenuLayout swipeMenuLayout = findViewById(R.id.swipe_menu_layout);
+        //设置侧滑是否可用
         swipeMenuLayout.setSwipeEnable(true);
+        //设置侧滑状态变化监听
         swipeMenuLayout.addOnMenuStateChangeListener(new SwipeMenuLayout.OnMenuStateChangeListener() {
             @Override
             public void onOpenMenu() {
+                //侧滑打开
                 swipeMenuLayout.smoothClose();
                 startActivity(new Intent(MainActivity.this, LazyModeActivity.class));
             }
 
             @Override
             public void onCloseMenu() {
+                //侧滑关闭
             }
         });
         LazyModeEntryView lazyModeEntryView = findViewById(R.id.lazy_mode_entry_view);
